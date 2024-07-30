8b6894e08ad1d56cfef85f2471c361a706f5ee36
ThataHi <91163255+ThataHi@users.noreply.github.com>
2024-07-29 21:00:25 -0300

    Delete Atividade2/src/Ex3_Math directory

--- Atividade2/src/Ex3_Math/ex3_i_hipotenusa.java
+++
@@ -1,9 +0,0 @@
-package Ex3_Math;
-
-public class ex3_i_hipotenusa {
-	public static double calculaHipotenusa(double cateto1, double cateto2) {
-        // Aplica o teorema de Pitágoras
-        double hipotenusa = Math.sqrt(cateto1 * cateto1 + cateto2 * cateto2);
-        return hipotenusa;
-    }
-}
--- Atividade2/src/Ex3_Math/ex3_ii.java
+++
@@ -1,9 +0,0 @@
-package Ex3_Math;
-
-public class ex3_ii {
-	public static void main(String[] args) {
-        int randomNumber = ex3_ii_numeroAleatorio.generateRandomNumber();
-
-        System.out.println("Número aleatório gerado: " + randomNumber);
-    }
-}
